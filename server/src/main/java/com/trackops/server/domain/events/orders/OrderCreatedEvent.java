bfb636244ebe5cfc23fccd89c0070889e486230c
William Lam <williamntlam@gmail.com>
2025-06-23 18:53:58 -0400

    feat: implemented created order event class

---
+++ server/src/main/java/com/trackops/server/domain/events/orders/OrderCreatedEvent.java
@@ -0,0 +1,20 @@
+package com.trackops.server.domain.events.orders;
+
+public class OrderCreatedEvent extends OrderEvent {
+
+    private final String createdBy;
+
+    public OrderCreatedAt(UUID orderId, String createdBy) {
+
+        super(orderId, "orderCreated")
+        this.createdBy = createdBy;
+
+    }
+
+    public String getCreatedBy() {
+
+        return createdBy;
+
+    }
+
+}
