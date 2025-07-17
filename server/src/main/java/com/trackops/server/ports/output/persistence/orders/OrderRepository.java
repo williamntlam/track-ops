39939d7b0ef4649030ee072e821221b3a31709bc
William Lam <williamntlam@gmail.com>
2025-07-16 20:34:53 -0400

    feat: implemented OrderRepository.java port

--- server/src/main/java/com/trackops/server/ports/output/persistence/orders/OrderRepository.java
+++ server/src/main/java/com/trackops/server/ports/output/persistence/orders/OrderRepository.java
@@ -1,7 +1,15 @@
-public class OrderRepository {
+import com.trackops.server.domain.model.orders.Order;
+import com.trackops.server.domain.model.enums.OrderStatus;
+import java.util.Optional;
+import java.util.List;
 
-    public OrderRepository() {
-        
-    }
+public interface OrderRepository {
+
+    Order save(Order order);
+    Optional<Order> findById(Long id);
+    List<Order> findByStatus(OrderStatus status);
+    List<Order> findAll();
+    void deleteById(Long id);
+    boolean existsById(Long id);
 
 }
