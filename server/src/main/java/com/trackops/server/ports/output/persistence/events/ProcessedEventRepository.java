ccd8027e0ab8870352c3e44509df048ae37be0c0
William Lam <williamntlam@gmail.com>
2025-07-16 20:38:13 -0400

    feat: implemented ProcessedEventRepository.java

--- server/src/main/java/com/trackops/server/ports/output/persistence/events/ProcessedEventRepository.java
+++ server/src/main/java/com/trackops/server/ports/output/persistence/events/ProcessedEventRepository.java
@@ -1,7 +1,10 @@
-public class ProcessedEventRepository {
+import com.trackops.server.domain.model.events.ProcessedEvent;
+import java.util.Optional;
 
-    public ProcessedEventRepository() {
-        
-    }
+public interface ProcessedEventRepository {
 
+    ProcessedEvent save(ProcessedEvent processedEvent);
+    Optional<ProcessedEvent> findByEventId(String eventId);
+    boolean existsByEventId(String eventId);
+    void deleteByEventId(String eventId);
 }
