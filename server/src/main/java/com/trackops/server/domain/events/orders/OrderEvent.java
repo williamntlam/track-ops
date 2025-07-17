4c619d8270aef0460c5887955a8b564ab5a249e9
William Lam <williamntlam@gmail.com>
2025-07-16 20:16:59 -0400

    refactor: made OrderEvent an abstract class that inherits from Event.java

--- server/src/main/java/com/trackops/server/domain/events/orders/OrderEvent.java
+++ server/src/main/java/com/trackops/server/domain/events/orders/OrderEvent.java
@@ -1,38 +1,21 @@
 package com.trackops.server.domain.events.orders;
 
-import java.time.Instant;
+import com.trackops.server.domain.model.events.Event;
 import java.util.UUID;
 
-public abstract class OrderEvent {
+public abstract class OrderEvent extends Event {
 
-    private final UUID eventId;
     private final UUID orderId;
-    private final Instant timestamp;
-    private final String eventType;
 
     public OrderEvent(UUID orderId, String eventType) {
 
-        this.eventId = UUID.randomUUID(); // There might be a problem here.
-        this.orderId = orderId;
-        this.timestamp = Instant.now();
-        this.eventType = eventType;
+        super(eventType);
+        this.orderId = orderId; 
 
-    }
-
-    public UUID getEventId() {
-        return eventId;
     }
 
     public UUID getOrderId() {
         return orderId;
-    }
-
-    public Instant getTimestamp() {
-        return timestamp;
-    }
-
-    public String getEventType() {
-        return eventType;
     }
 
 }
