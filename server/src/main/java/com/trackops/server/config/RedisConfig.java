e59a9305b19c8bbb148e702a5e7a05d5aded89c0
William Lam <williamntlam@gmail.com>
2025-07-19 20:37:39 -0400

    feat: implemented RedisConfig.java

--- server/src/main/java/com/trackops/server/config/RedisConfig.java
+++ server/src/main/java/com/trackops/server/config/RedisConfig.java
@@ -1,3 +1,47 @@
 package com.trackops.server.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
 @Configuration
+public class RedisConfig {
+
+    /**
+     * Create RedisConnectionFactory with RedisStandaloneConfiguration.
+     * Host: localhost, Port: 6379, DB: 0
+     */
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName("localhost");
+        config.setPort(6379);
+        config.setDatabase(0); // Optional: default is 0
+        return new LettuceConnectionFactory(config);
+    }
+
+    /**
+     * Create RedisTemplate<String, String> with String serializers.
+     */
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // Configure serializers
+        StringRedisSerializer stringSerializer = StringRedisSerializer.UTF_8;
+
+        template.setKeySerializer(stringSerializer);
+        template.setValueSerializer(stringSerializer);
+        template.setHashKeySerializer(stringSerializer);
+        template.setHashValueSerializer(stringSerializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
+}
