e3b12fbe4cbc9759c31dcb099b6df65b6b80a6bc
William Lam <williamntlam@gmail.com>
2025-07-22 17:04:35 -0400

    feat: initalized constructors and attributes for the address DTO

--- server/src/main/java/com/trackops/server/adapters/input/web/dto/AddressDTO.java
+++ server/src/main/java/com/trackops/server/adapters/input/web/dto/AddressDTO.java
@@ -2,6 +2,24 @@ package com.trackops.server.adapters.input.web.dto;
 
 public class AddressDTO {
 
+    private String streetAddress;
+    private String city;
+    private String postalCode;
+    private String country;
+    private String phoneNumber;
 
+    public AddressDTO() {
+
+    }
+
+    public AddressDTO(String streetAddress, String city, String postalCode, String country, String phoneNumber) {
+
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.postalCode = postalCode;
+        this.country = country;
+        this.phoneNumber = phoneNumber; 
+
+    }
 
 }
