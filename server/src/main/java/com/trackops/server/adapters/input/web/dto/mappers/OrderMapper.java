76d8e0db83400a69b7e330a9ec3e75b876132787
William Lam <williamntlam@gmail.com>
2025-07-23 17:12:22 -0400

    feat: initialized skeleton methods for order mapper

--- server/src/main/java/com/trackops/server/adapters/input/web/dto/mappers/OrderMapper.java
+++ server/src/main/java/com/trackops/server/adapters/input/web/dto/mappers/OrderMapper.java
@@ -4,10 +4,29 @@ import com.trackops.server.adapters.input.web.dto.AddressDTO;
 import com.trackops.server.adapters.input.web.dto.CreateOrderRequest;
 import com.trackops.server.adapters.input.web.dto.OrderResponse;
 import com.trackops.server.domain.model.orders.Order;
-
+import com.trackops.server.domain.model.orders.Address;
+import java.util.List;
 
 public class OrderMapper {
 
+    // Order Object to OrderResponse
+    public OrderResponse toOrderResponse(Order order) {
 
+    }
+
+    // CreateOrderRequest to Order Object
+    public OrderResponse toOrder(CreateOrderRequest request) {
+
+    }
+
+    // AddressDTO to Address Object
+    public Address toAddress(AddressDTO dto) {
+
+    }
+
+    // Map of a list of Order objects to a list of OrderResponse
+    public List<OrderResponse> toOrderResponseList(List<Order> orders) {
+
+    }
 
 }
