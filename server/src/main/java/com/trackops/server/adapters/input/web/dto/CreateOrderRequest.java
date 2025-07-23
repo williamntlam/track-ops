2ab6146ad0e9301ddbd078e42ca2f584420eb3e5
William Lam <williamntlam@gmail.com>
2025-07-23 15:01:50 -0400

    feat: added getters, setters and override methods for CreatOrderRequest DTO

--- server/src/main/java/com/trackops/server/adapters/input/web/dto/CreateOrderRequest.java
+++ server/src/main/java/com/trackops/server/adapters/input/web/dto/CreateOrderRequest.java
@@ -31,4 +31,61 @@ public class CreateOrderRequest {
         this.deliveryInstructions = deliveryInstructions;
     }
 
+    public UUID getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(UUID customerId) {
+        this.customerId = customerId;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public AddressDTO getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressDTO address) {
+        this.address = address;
+    }
+
+    public String getDeliveryInstructions() {
+        return deliveryInstructions;
+    }
+
+    public void setDeliveryInstructions(String deliveryInstructions) {
+        this.deliveryInstructions = deliveryInstructions;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateOrderRequest{" +
+                "customerId=" + customerId +
+                ", totalAmount=" + totalAmount +
+                ", address=" + address +
+                ", deliveryInstructions='" + deliveryInstructions + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CreateOrderRequest that = (CreateOrderRequest) o;
+        return java.util.Objects.equals(customerId, that.customerId) &&
+                java.util.Objects.equals(totalAmount, that.totalAmount) &&
+                java.util.Objects.equals(address, that.address) &&
+                java.util.Objects.equals(deliveryInstructions, that.deliveryInstructions);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(customerId, totalAmount, address, deliveryInstructions);
+    }
 }
