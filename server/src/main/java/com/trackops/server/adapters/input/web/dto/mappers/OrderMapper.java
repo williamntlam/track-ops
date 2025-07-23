8424e574d2a7843da4a1a94808be5d511ec04dec
William Lam <williamntlam@gmail.com>
2025-07-23 16:57:24 -0400

    feat: implemented Address domain model and initialized and created DTO mapper file

---
+++ server/src/main/java/com/trackops/server/adapters/input/web/dto/mappers/OrderMapper.java
@@ -0,0 +1,13 @@
+package com.trackops.server.adapters.input.web.dto.mappers;
+import com.trackops.server.adapters.input.web.dto.OrderResponse;
+import com.trackops.server.adapters.input.web.dto.AddressDTO;
+import com.trackops.server.adapters.input.web.dto.CreateOrderRequest;
+import com.trackops.server.adapters.input.web.dto.OrderResponse;
+import com.trackops.server.domain.model.orders.Order;
+
+
+public class OrderMapper {
+
+
+
+}
---
+++ server/src/main/java/com/trackops/server/domain/model/orders/Address.java
@@ -0,0 +1,101 @@
+package com.trackops.server.domain.model.orders;
+
+import java.util.Objects;
+
+public class Address {
+    private String streetAddress;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+    private String phoneNumber;
+
+    public Address() {}
+
+    public Address(String streetAddress, String city, String state, String postalCode, String country, String phoneNumber) {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+        this.postalCode = postalCode;
+        this.country = country;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "streetAddress='" + streetAddress + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", country='" + country + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(streetAddress, address.streetAddress) &&
+                Objects.equals(city, address.city) &&
+                Objects.equals(state, address.state) &&
+                Objects.equals(postalCode, address.postalCode) &&
+                Objects.equals(country, address.country) &&
+                Objects.equals(phoneNumber, address.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(streetAddress, city, state, postalCode, country, phoneNumber);
+    }
+} 
