e9f1ad8dbc1ea0113a5cab68ae1aec3ca0371417
William Lam <williamntlam@gmail.com>
2025-07-23 14:15:21 -0400

    feat: added validation otations and toString(),  equals() and hashCode() for debugging purposes

--- server/src/main/java/com/trackops/server/adapters/input/web/dto/AddressDTO.java
+++ server/src/main/java/com/trackops/server/adapters/input/web/dto/AddressDTO.java
@@ -1,27 +1,43 @@
 package com.trackops.server.adapters.input.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class AddressDTO {
 
+    @NotBlank
+    @Size(max = 255)
     private String streetAddress;
+
+    @NotBlank
+    @Size(max = 100)
     private String city;
+
+    @Size(max = 100)
+    private String state;
+
+    @NotBlank
+    @Size(max = 20)
     private String postalCode;
+
+    @NotBlank
+    @Size(max = 100)
     private String country;
-    private string state;
+
+    @Size(max = 50)
     private String phoneNumber;
 
     public AddressDTO() {
 
     }
 
-    public AddressDTO(String streetAddress, String city, String postalCode, String country, String state, String phoneNumber) {
-
+    public AddressDTO(String streetAddress, String city, String state, String postalCode, String country, String phoneNumber) {
         this.streetAddress = streetAddress;
         this.city = city;
+        this.state = state;
         this.postalCode = postalCode;
         this.country = country;
-        this.state = state;
         this.phoneNumber = phoneNumber; 
-
     }
 
     public String getStreetAddress() {
@@ -40,6 +56,14 @@ public class AddressDTO {
         this.city = city;
     }
 
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
     public String getPostalCode() {
         return postalCode;
     }
@@ -56,14 +80,6 @@ public class AddressDTO {
         this.country = country;
     }
 
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
     public String getPhoneNumber() {
         return phoneNumber;
     }
@@ -72,4 +88,33 @@ public class AddressDTO {
         this.phoneNumber = phoneNumber;
     }
 
+    @Override
+    public String toString() {
+        return "AddressDTO{" +
+                "streetAddress='" + streetAddress + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", country='" + country + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AddressDTO that = (AddressDTO) o;
+        return java.util.Objects.equals(streetAddress, that.streetAddress) &&
+                java.util.Objects.equals(city, that.city) &&
+                java.util.Objects.equals(state, that.state) &&
+                java.util.Objects.equals(postalCode, that.postalCode) &&
+                java.util.Objects.equals(country, that.country) &&
+                java.util.Objects.equals(phoneNumber, that.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(streetAddress, city, state, postalCode, country, phoneNumber);
+    }
 }
