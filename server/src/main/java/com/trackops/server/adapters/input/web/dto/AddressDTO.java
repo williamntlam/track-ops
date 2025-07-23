c94836c41c07a3e2dbc4a6325757ab76f623c9b0
William Lam <williamntlam@gmail.com>
2025-07-23 13:15:30 -0400

    feat: added getters and setters for Address DTO

--- server/src/main/java/com/trackops/server/adapters/input/web/dto/AddressDTO.java
+++ server/src/main/java/com/trackops/server/adapters/input/web/dto/AddressDTO.java
@@ -22,4 +22,47 @@ public class AddressDTO {
 
     }
 
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    // this.country = country;
+    // this.phoneNumber = phoneNumber; 
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
 }
