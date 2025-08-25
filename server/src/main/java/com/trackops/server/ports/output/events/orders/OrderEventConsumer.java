1117cccee875c6b76cb44b4bbaf209f67753462a
William Lam <williamntlam@gmail.com>
2025-08-25 18:49:51 -0400

    feat: implemented interface for OrderEventConsumer

--- server/src/main/java/com/trackops/server/ports/output/events/orders/OrderEventConsumer.java
+++ server/src/main/java/com/trackops/server/ports/output/events/orders/OrderEventConsumer.java
@@ -1,3 +1,17 @@
 package com.trackops.server.ports.output.events.orders;
 
+import com.trackops.server.domain.events.orders.OrderCreatedEvent;
+import com.trackops.server.domain.events.orders.OrderStatusUpdatedEvent;
+import com.trackops.server.domain.events.orders.OrderDeliveredEvent;
+import com.trackops.server.domain.events.orders.OrderCancelledEvent;
 
+public interface OrderEventConsumer {
+
+    void consumeOrderCreated(OrderCreatedEvent event);
+    void consumeOrderStatusUpdated(OrderStatusUpdatedEvent event);
+    void consumeOrderDelivered(OrderDeliveredEvent event);
+    void consumeOrderCancelled(OrderCancelledEvent event);
+    void processEvent(String eventJson, String topic);
+    void handleEventProcessingError(Object event, Exception error);
+
+}
